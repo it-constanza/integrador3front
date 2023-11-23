7b88ff3d790682e9e204c1f29d834be242335137
Cony Feldman <it.constanza@gmail.com>
2023-11-23 03:08:13 -0300

    Header sin barra de busqueda y ajustes botones TablaFila

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,13 @@ import { Link } from "react-router-dom";
 import "./Header.scss";
 import Navbar from "./Navbar";
 
+
 const Header = () => {
   return (
     <header className="main-header">
       <input type="checkbox" id="menu" />
       <Navbar />
       <div className="search-bar">
-
-        <form action="" className="search-bar__form-container">
-          <label htmlFor="busqueda" className="search-bar__form-label">
-            Buscar
-          </label>
-          <input type="search" className="search-bar__form-search" id="busqueda" />
-          <input type="submit" value="Buscar" className="search-bar__form-submit" />
-        </form>
         <Link className="search-bar__carrito-container" to="/carrito"> <img src="/img/bolsavacia.png" alt="" /> </Link>
         <div className="menu-toggle">
           <label htmlFor="menu" className="menu-toggle__label">
